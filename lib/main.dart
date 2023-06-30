a0c960ade6366c64c7eb69b5acd4781f04f2ef4c
kareembelal82 <88692979+kareembelal82@users.noreply.github.com>
2023-06-30 16:47:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/features/splash/presentaion/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
